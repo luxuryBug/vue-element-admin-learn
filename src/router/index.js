1f7acd9aa0bd1e404a51cc26fe48b33b01254d88
guoluo <guoluo@acewill.cn>
2017-06-01 18:28:55 +0800

    2.创建login页面,todo:Expected linebreaks to be 'LF' but found 'CRLF'

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,15 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
 
+/* Login */
+import Login from '../views/login/index.vue'
+
 Vue.use(Router)
+
 
 export default new Router({
   routes: [
+		{ path: '/login', component: Login, hidden: true },
     {
       path: '/',
       name: 'Hello',
---
+++ src/views/login/index.vue
@@ -0,0 +1,63 @@
+<template>
+	<div class="login-container">
+		<el-form :model="loginForm" :rules="loginRules" ref="loginForm">
+			<el-form-item>
+				<el-input v-model="loginForm.email" placeholder="邮箱"></el-input>
+			</el-form-item>
+			<el-form-item>
+				<el-input v-model="loginForm.password" placeholder="密码"></el-input>
+			</el-form-item>
+			<el-form-item>
+				<el0-button type="primary">登录</el-button>
+			</el-form-item>
+		</el-form>
+	</div>
+</template>
+
+<script>
+
+
+	export default {
+		name: 'login',
+		data: function(){
+			// 验证邮箱
+			const validateEmail = (rule,value,callback) => {
+				if(!(value)){
+					callback(new Error('请输入正确的合法邮箱'))
+				}else{
+					callback()
+				}
+			};
+			// 验证邮箱
+			const validatePassword = (rule,value,callback) => {
+				if(value.length < 6){
+					callback(new Error('密码不能小于6位'))
+				}else{
+					callback()
+				}
+			};
+			return {
+				loginForm: {
+					email: 'xx@qq.com',
+					password: ''
+				},
+				loginRules: {
+					email: [
+						{
+							required: true,
+							trigger: 'blur',
+							validator: validateEmail
+						}
+					],
+					password: [
+						{
+							required: true,
+							trigger: 'blur',
+							validator: validatePassword
+						}
+					],
+				}
+			}
+		}
+	}
+</script>
